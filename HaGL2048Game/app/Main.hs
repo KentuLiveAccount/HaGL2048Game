3a60f8f20dc18ad16db92cde641d678e28613e22
Kentaro Urata <kentu@microsoft.com>
2024-12-06 20:49:20 -0800

    move tile functions to library

--- HaGL2048Game/app/Main.hs
+++ HaGL2048Game/app/Main.hs
@@ -4,12 +4,8 @@ import Lib
 
 import Graphics.UI.GLUT
 import Data.IORef
-import Data.List (sortBy)
 
 divisor = 24
-
-grids :: Int
-grids = 4
 
 wh :: GLfloat
 wh = ((2::GLfloat) / (fromIntegral grids))
@@ -21,12 +17,6 @@ wh = ((2::GLfloat) / (fromIntegral grids))
    centers = increments + 2 increments * (i)  (where 0 <=i < n)
    
 -}
-
-gridPosToPos :: Int -> GLfloat
-gridPosToPos v = -1 + increments + 2 * increments * (fromIntegral v)
-  where
-    increments :: GLfloat
-    increments = 2 / (2 * (fromIntegral grids))
 
 
 sign :: (Ord a, Num a) => a -> a
@@ -43,18 +33,6 @@ toRect (x1, y1, z1) = [(x - hwh, y - hwh, z), (x +  hwh, y - hwh, z), (x + hwh, 
 color3f :: GLfloat -> GLfloat -> GLfloat -> IO ()
 color3f r g b = color $ Color3 r g b
 
-data Tile = TL {pos :: (Int, Int)} deriving (Eq)
-
-data LinearMotion = LM {
-    cur :: (GLfloat, GLfloat),
-    dest :: (GLfloat, GLfloat)
-}
-
-stillMotion :: Tile -> LinearMotion
-stillMotion (TL (x, y)) =LM (gridPosToPos x, gridPosToPos y) (gridPosToPos x, gridPosToPos y)
-
-enMotion :: Tile -> Tile -> LinearMotion
-enMotion (TL (xs, ys)) (TL (xd, yd)) =LM (gridPosToPos xs, gridPosToPos ys) (gridPosToPos xd, gridPosToPos yd)
 
 anyMotion :: [LinearMotion] -> Bool
 anyMotion lms = any f lms
@@ -80,45 +58,7 @@ moveDestLm (xm, ym) (LM (cx, cy) (dx, dy)) = (LM (cx, cy) (dx', dy'))
     newValue :: Int -> GLfloat -> GLfloat -> GLfloat
     newValue m cur dest = if ((abs m) == 0) then dest else (if ((sign m) < 0) then (gridPosToPos 0) else (gridPosToPos 3))
 
-moveTiles :: (Int, Int) -> [Tile] -> ([Tile], [LinearMotion], Bool)
-moveTiles dir@(xm, ym) tiles = (tiles', zipWith (enMotion) tilesSorted tiles', tilesSorted == tiles')
-  where
-    tilesSorted = sortBy (sortDir dir) tiles
-    tiles' = concatMap (moveDir dir) $ splitDir dir tilesSorted
 
-    sortDir :: (Int, Int) -> Tile -> Tile -> Ordering
-    sortDir (-1,  0) (TL (xa, ya)) (TL (xb, yb)) = if (xa == xb) then (compare ya yb) else (compare xa xb)
-    sortDir ( 1,  0) (TL (xa, ya)) (TL (xb, yb)) = if (xa == xb) then (compare ya yb) else (compare xb xa)
-    sortDir ( 0, -1) (TL (xa, ya)) (TL (xb, yb)) = if (ya == yb) then (compare xa xb) else (compare ya yb)
-    sortDir ( 0,  1) (TL (xa, ya)) (TL (xb, yb)) = if (ya == yb) then (compare xa xb) else (compare yb ya)
-    sortDir (_, _) _ _ = EQ
-   
-    splitDir :: (Int, Int) -> [Tile] -> [[Tile]]
-    splitDir _ [] = [[]]
-    splitDir _ [t] = [[t]]
-    splitDir dir (t:tls) = (t:ms): (splitDir dir us)
-      where
-        (ms, us) = span (predDir dir t) tls
-
-    predDir :: (Int, Int) -> Tile -> Tile -> Bool
-    predDir (-1,  0) (TL (x1, _)) (TL (x2, _)) = x1 == x2
-    predDir ( 1,  0) (TL (x1, _)) (TL (x2, _)) = x1 == x2
-    predDir ( 0, -1) (TL (_, y1)) (TL (_, y2)) = y1 == y2
-    predDir ( 0,  1) (TL (_, y1)) (TL (_, y2)) = y1 == y2
-
-    moveDir :: (Int, Int) -> [Tile] -> [Tile]
-    moveDir dir tls = zipWith (fn) tls (dirNum dir)
-      where
-        fn :: Tile -> (Int, Int) -> Tile
-        fn (TL (x, y)) (xn, 0) = TL (xn, y)
-        fn (TL (x, y)) (0, yn) = TL (x, yn)
-        fn (TL (x, y)) (0, 0) = TL (x, y)
-
-    dirNum :: (Int, Int) -> [(Int, Int)]
-    dirNum (1, 0) = [(x, 0) | x <- [(grids - 1)..0]]
-    dirNum (-1, 0) = [(x, 0) | x <- [0..(grids - 1)]]
-    dirNum (0, 1) = [(0, y) | y <- [(grids - 1)..0]]
-    dirNum (0, -1) = [(0, y) | y <- [0..(grids - 1)]]
 
 data AppState = AS {
   tiles :: [Tile],
@@ -129,7 +69,8 @@ data AppState = AS {
 initialAppState :: AppState
 initialAppState = AS tiles (initialLMs tiles) 0
   where
-    tiles = [TL (0, 3), TL (0, 2)]
+    tiles = [TL (0, 3)]
+--    tiles = [TL (0, 3), TL (0, 2)]
     initialLMs :: [Tile] -> [LinearMotion]
     initialLMs  = map (stillMotion)
 
@@ -138,6 +79,13 @@ onMotion f (AS tls lms to) = AS tls lms' to'
   where
     lms' = if (to == 0) then (f lms) else lms
     to' = if (anyMotion lms') then 16 else 0
+
+--moveTiles :: (Int, Int) -> [Tile] -> ([Tile], [LinearMotion], Bool)
+
+onMove :: (Int, Int) -> AppState -> AppState
+onMove mv (AS tls lms to) = (AS tls' lms (if f then 16 else 0))
+    where
+      (tls', lms, f) = moveTiles mv tls
 
 onTime :: AppState -> AppState
 onTime (AS tls lms to) = (AS tls lms' to')
@@ -196,11 +144,12 @@ timerProc ior = do
     postRedisplay Nothing
     modifyIORef ior onTime
 
+
 -- ($~!) :: MonadIO m => t -> (a -> b) -> m () 
 keyboardMouse :: IORef AppState -> KeyboardMouseCallback
 keyboardMouse ior key Down _ _ = case key of
-  (SpecialKey KeyLeft ) -> ior $~! onMotion (map (moveDestLm (-1,  0))) >> updateTimer ior
-  (SpecialKey KeyRight) -> ior $~! onMotion (map (moveDestLm ( 1,  0))) >> updateTimer ior
+  (SpecialKey KeyLeft ) -> ior $~! onMove (-1, 0) >> updateTimer ior
+  (SpecialKey KeyRight) -> ior $~! onMove ( 1, 0) >> updateTimer ior
   (SpecialKey KeyUp   ) -> ior $~! onMotion (map (moveDestLm ( 0,  1))) >> updateTimer ior
   (SpecialKey KeyDown ) -> ior $~! onMotion (map (moveDestLm ( 0, -1))) >> updateTimer ior
   _ -> return ()
--- HaGL2048Game/src/Lib.hs
+++ HaGL2048Game/src/Lib.hs
@@ -1,6 +1,81 @@
-module Lib
-    ( someFunc
+module Lib (
+    grids,
+    gridPosToPos,
+    moveTiles,
+    stillMotion,
+    enMotion,
+    predDir,
+    splitDir,
+    sortDir,
+    moveDir,
+    dirNum,
+    Tile (..),
+    LinearMotion (..)
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Graphics.UI.GLUT (GLfloat)
+import Data.List (sortBy)
+
+data Tile = TL {pos :: (Int, Int)} deriving (Eq)
+
+data LinearMotion = LM {
+    cur :: (GLfloat, GLfloat),
+    dest :: (GLfloat, GLfloat)
+}
+
+grids :: Int
+grids = 4
+
+
+gridPosToPos :: Int -> GLfloat
+gridPosToPos v = -1 + increments + 2 * increments * (fromIntegral v)
+  where
+    increments :: GLfloat
+    increments = 2 / (2 * (fromIntegral grids))
+
+
+stillMotion :: Tile -> LinearMotion
+stillMotion (TL (x, y)) =LM (gridPosToPos x, gridPosToPos y) (gridPosToPos x, gridPosToPos y)
+
+enMotion :: Tile -> Tile -> LinearMotion
+enMotion (TL (xs, ys)) (TL (xd, yd)) =LM (gridPosToPos xs, gridPosToPos ys) (gridPosToPos xd, gridPosToPos yd)
+
+predDir :: (Int, Int) -> Tile -> Tile -> Bool
+predDir (-1,  0) (TL (x1, _)) (TL (x2, _)) = x1 == x2
+predDir ( 1,  0) (TL (x1, _)) (TL (x2, _)) = x1 == x2
+predDir ( 0, -1) (TL (_, y1)) (TL (_, y2)) = y1 == y2
+predDir ( 0,  1) (TL (_, y1)) (TL (_, y2)) = y1 == y2
+
+splitDir :: (Int, Int) -> [Tile] -> [[Tile]]
+splitDir _ [] = [[]]
+splitDir _ [t] = [[t]]
+splitDir dir (t:tls) = (t:ms): (splitDir dir us)
+    where
+    (ms, us) = span (predDir dir t) tls
+
+sortDir :: (Int, Int) -> Tile -> Tile -> Ordering
+sortDir (-1,  0) (TL (xa, ya)) (TL (xb, yb)) = if (xa == xb) then (compare ya yb) else (compare xa xb)
+sortDir ( 1,  0) (TL (xa, ya)) (TL (xb, yb)) = if (xa == xb) then (compare ya yb) else (compare xb xa)
+sortDir ( 0, -1) (TL (xa, ya)) (TL (xb, yb)) = if (ya == yb) then (compare xa xb) else (compare ya yb)
+sortDir ( 0,  1) (TL (xa, ya)) (TL (xb, yb)) = if (ya == yb) then (compare xa xb) else (compare yb ya)
+sortDir (_, _) _ _ = EQ
+
+moveDir :: (Int, Int) -> [Tile] -> [Tile]
+moveDir dir tls = zipWith (fn) tls (dirNum dir)
+    where
+    fn :: Tile -> (Int, Int) -> Tile
+    fn (TL (x, y)) (0, 0) = TL (x, y)
+    fn (TL (x, y)) (xn, 0) = TL (xn, y)
+    fn (TL (x, y)) (0, yn) = TL (x, yn)
+
+dirNum :: (Int, Int) -> [(Int, Int)]
+dirNum (1, 0) = [(x, 0) | x <- [(grids - 1)..0]]
+dirNum (-1, 0) = [(x, 0) | x <- [0..(grids - 1)]]
+dirNum (0, 1) = [(0, y) | y <- [(grids - 1)..0]]
+dirNum (0, -1) = [(0, y) | y <- [0..(grids - 1)]]
+
+moveTiles :: (Int, Int) -> [Tile] -> ([Tile], [LinearMotion], Bool)
+moveTiles dir tiles = (tiles', zipWith (enMotion) tilesSorted tiles', tilesSorted /= tiles')
+  where
+    tilesSorted = sortBy (sortDir dir) tiles
+    tiles' = concatMap (moveDir dir) $ splitDir dir tilesSorted
