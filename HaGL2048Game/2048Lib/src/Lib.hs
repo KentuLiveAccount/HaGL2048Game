7df03ef69530de917f73e619441e3dd269908b9e
Kentaro Urata <kentu@microsoft.com>
2025-04-06 22:27:25 -0700

    tweak implementaion

--- HaGL2048Game/2048Lib/src/Lib.hs
+++ HaGL2048Game/2048Lib/src/Lib.hs
@@ -14,22 +14,33 @@ module Lib (
     ) where
 
 import Graphics.UI.GLUT (GLfloat)
+import Control.Monad(when)
+import Data.Function (on)
 import Data.List (sortBy, groupBy)
-import Data.Tuple (swap)
+import Data.Tuple (swap, fst, snd)
+
+orNothing :: Bool -> a -> Maybe a
+orNothing True a = Just a
+orNothing False _ = Nothing
+
+(?) :: Bool -> a -> a -> a 
+(?) True a _ = a
+(?) False _ a = a
 
 data Tile = TL {pos :: (Int, Int), val :: Int} deriving (Eq, Show)
 
 swapXY :: Tile -> Tile
-swapXY (TL (x, y) v) = TL (y, x) v
+swapXY tl = tl {pos = swap $ pos tl}
 
+-- Data.Function.on :: (b -> b -> c) -> (a -> b) -> a -> a -> c
 samePos :: Tile -> Tile -> Bool
-samePos t1 t2 = (pos t1) == (pos t2)
+samePos = on (==) pos
 
 sameVal :: Tile -> Tile -> Bool
-sameVal t1 t2 = (val t1) == (val t2)
+sameVal = on (==) val
 
 data LinearMotion = LM {
-    cur :: (GLfloat, GLfloat),
+    cur  :: (GLfloat, GLfloat),
     dest :: (GLfloat, GLfloat),
     valCur :: Int,
     valDest :: Int
@@ -37,7 +48,6 @@ data LinearMotion = LM {
 
 grids :: Int
 grids = 4
-
 
 gridPosToPos :: Int -> GLfloat
 gridPosToPos v = -1 + increments + 2 * increments * (fromIntegral v)
@@ -53,9 +63,9 @@ enMotion :: Tile -> Tile -> LinearMotion
 enMotion (TL (xs, ys) vs) (TL (xd, yd) vd) = LM (gridPosToPos xs, gridPosToPos ys) (gridPosToPos xd, gridPosToPos yd) vs vd
 
 predDir :: (Int, Int) -> Tile -> Tile -> Bool
-predDir ( 0,  0) (TL (x1, _) _) (TL (x2, _) _) = True
-predDir ( 0,  _) (TL (x1, _) _) (TL (x2, _) _) = x1 == x2
-predDir ( _,  0) (TL (_, y1) _) (TL (_, y2) _) = y1 == y2
+predDir ( 0,  0) = \_ _ -> True
+predDir ( 0,  _) = on (==) (fst.pos)
+predDir ( _,  0) = on (==) (snd.pos)
 
 foo :: (a -> a -> Maybe [a]) -> [a] -> [a]
 foo _ [] = []
@@ -72,8 +82,8 @@ splitDir dir (t:tls) = (t:ms): (splitDir dir us)
     (ms, us) = span (predDir dir t) tls
 
 compareDir :: (Int, Int) -> Tile -> Tile -> Ordering
-compareDir ( 0,  1) (TL (xa, ya) _) (TL (xb, yb) _) = if (xa == xb) then (compare yb ya) else (compare xa xb)
-compareDir ( 0, -1) (TL (xa, ya) _) (TL (xb, yb) _) = if (xa == xb) then (compare ya yb) else (compare xa xb)
+compareDir ( 0,  1) (TL (xa, ya) _) (TL (xb, yb) _) = (xa == xb) ? (compare yb ya) $ (compare xa xb)
+compareDir ( 0, -1) (TL (xa, ya) _) (TL (xb, yb) _) = (xa == xb) ? (compare ya yb) $ (compare xa xb)
 compareDir ( 1,  0) t1 t2 = compareDir (0,  1) (swapXY t1) (swapXY t2)
 compareDir (-1,  0) t1 t2 = compareDir (0, -1) (swapXY t1) (swapXY t2)
 compareDir (_, _) _ _ = EQ
@@ -93,12 +103,12 @@ dirNum ( 0,  1) = map swap $ dirNum (1, 0)
 dirNum ( 0, -1) = reverse  $ dirNum (0, 1)
 
 consolidateTiles :: [Tile] -> [Tile]
-consolidateTiles = foo (\t1 t2 -> if (sameVal t1 t2) then Just [doubleVal t1, doubleVal t1] else Nothing)
+consolidateTiles = foo (\t1 t2 -> orNothing (sameVal t1 t2) [doubleVal t1, doubleVal t1])
     where
-        doubleVal (TL pos v) = TL pos (v+v)
+        doubleVal tl = tl {val = 2 * (val tl)}
 
 filterDuplicate :: [Tile] -> [Tile]
-filterDuplicate = foo (\a1 a2 -> if (pos a1) == (pos a2) then Just [a2] else Nothing)
+filterDuplicate = foo (\a1 a2 -> orNothing (samePos a1 a2) [a2])
 
 findNewPos :: [Tile] -> [Int] -> ((Int, Int), [Int])
 findNewPos tiles ints = ((i `div` grids, i `mod` grids), is)
@@ -109,10 +119,10 @@ findNewPos tiles ints = ((i `div` grids, i `mod` grids), is)
         reject i = any (== (i `mod` (grids * grids))) poss
 
 moveTiles :: [Int] -> (Int, Int) -> [Tile] -> ([Tile], [LinearMotion], Bool, [Int])
-moveTiles news dir tiles = 
-    if (not changed) then (tiles, map stillMotion tiles, False, news)
-    else if (noNew) then (tiles'', lms, changed, news) 
-    else ((newTile : tiles'') ,(stillMotion newTile : lms), changed, news')
+moveTiles news dir tiles = (not changed) ?
+    (tiles, map stillMotion tiles, False, news) $
+        (noNew) ? (tiles'', lms, changed, news) $
+            ((newTile : tiles'') ,(stillMotion newTile : lms), changed, news')
   where
     tilesSorted = sortBy (compareDir dir) tiles
     tiles' = concat $ map (moveDir dir . consolidateTiles . moveDir dir) $ splitDir dir tilesSorted
